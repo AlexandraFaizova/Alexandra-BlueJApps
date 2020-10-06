dbe0675e4486bab2b3e463749bd6262393e86d93
AlexandraFaizova <72131290+AlexandraFaizova@users.noreply.github.com>
2020-10-06 19:55:16 +0100

    Lab class app 1 work Alex

---
+++ app01-lab-classes-copy/Course.java
@@ -0,0 +1,31 @@
+
+/**
+ * Course that students will enrol in.
+ *
+ * @author Alexandra Faizova
+ * @version 0.1
+ */
+public class Course
+{
+     public String title;
+     
+     public String CourseCode;
+
+    /**
+     * Constructor for objects of class Course
+     */
+    public Course()
+    {
+        this.CourseCode = CourseCode;
+        this.title = title;
+    }
+
+    /**
+     * Set the code for this Course.
+     */
+     
+     public void setCourseCode(String courseCode)
+    {
+        courseCode = courseCode;
+    }
+}
